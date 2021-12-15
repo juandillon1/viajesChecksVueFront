fb3dabf19e507e6d21ecc0e23d12e40d320577cb
unknown <juancruzdillon123@gmail.com>
2021-12-15 09:33:39 -0300

    Updates

--- src/URL_GLOBAL.js
+++
@@ -1,3 +0,0 @@
-export const URL_GLOBAL = {
-    URL_API : process.env.VUE_APP_URLAPI
-}
--- src/helpers/getApi.js
+++
@@ -1,9 +0,0 @@
-import { URL_GLOBAL } from "../URL_GLOBAL"
-
-export const getApi = async() => {
-    const {URL_API} = URL_GLOBAL;
-    const URL = `${URL_API}/`;
-    const resp = await fetch(URL)
-    const data = await resp.json()
-    return data
-}
